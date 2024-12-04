53f8b67b641e82b4ff44a3b8fd1e392f5b7fd88e
Jasonn <148473173+JasonFrz@users.noreply.github.com>
2024-12-04 13:44:08 +0700

    hans gay

---
+++ Proyek_PAD/Proyek_PAD/Form3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Proyek_PAD
+{
+    public partial class Form3 : Form
+    {
+        private string connectionString = "Server=localhost;Database=mcd_pad;Uid=root;Pwd=;";
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();
+            string password = textBox2.Text.Trim();
+
+            string query = "SELECT COUNT(1) FROM kasir WHERE nama_kasir = @username AND password_kasir = @password";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@password", password);
+
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (count == 1)
+                        {
+                            MessageBox.Show("Login berhasil!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            Cashier form1 = new Cashier();
+                            form1.Show();
+
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Username atau password salah.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Terjadi kesalahan: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
